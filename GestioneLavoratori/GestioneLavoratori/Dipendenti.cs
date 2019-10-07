c596a3efc82cce7b5070a9085929e1e43d800c48
PieroBagaggiolo <56152505+PieroBagaggiolo@users.noreply.github.com>
2019-10-07 17:59:26 +0200

    calcolo tasse dipendenti

--- GestioneLavoratori/GestioneLavoratori/Dipendenti.cs
+++ GestioneLavoratori/GestioneLavoratori/Dipendenti.cs
@@ -8,12 +8,38 @@ namespace GestioneLavoratori
 {
     class Dipendenti : Lavoratori
     {
+        public int RAL { get; set; }
         public int Tasse { get; set; }
         public int Mensilità { get; set; }
         public Dipendenti(string nome, string cognome) : base(nome, cognome)
         {
             Mensilità = 0;
-            Tasse = CalcoloStipendio(Mensilità);
+            Tasse = 0;
+            RAL = CalcoloStipendio(Mensilità);
+        }
+
+        public int CalcoloTasse()
+        {
+            if(0<RAL || RAL<6000)
+            {
+                return RAL;
+            }
+            else if(6000<RAL || RAL<15000)
+            {
+                return RAL / (15 / 100);
+            }
+            else if(15000<RAL || RAL<25000)
+            {
+                return RAL / (30 / 100);
+            }
+            else if(25000<RAL || RAL<=35000)
+            {
+                return RAL / (40 / 100);
+            }
+            else if(RAL>35000)
+            {
+                return RAL / (50 / 100);
+            }
         }
 
     }
