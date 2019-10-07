c310aaa72a558aef3bc76960cf937cf533ea69ee
PieroBagaggiolo <56152505+PieroBagaggiolo@users.noreply.github.com>
2019-10-07 16:49:23 +0200

    Update Persona.cs

--- GestioneLavoratori/GestioneLavoratori/Persona.cs
+++ GestioneLavoratori/GestioneLavoratori/Persona.cs
@@ -11,9 +11,22 @@ namespace GestioneLavoratori
         public string Nome { get; set; }
         public string Cognome { get; set; }
         public DateTime DataNascita { get; set; }
-
-        public string Sesso { get; set; }
+        public int Anni { get; set; }
+        public string Genere { get; set; }
 
         //public int Anni { get; set; }
+
+        public Persona(string nome, string cognome, DateTime dataNascita, string genere)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            dataNascita = DataNascita;
+            Genere = genere;
+
+            var AnnoAtt = DateTime.Now.Year;
+            var AnnoNascita = dataNascita.Year;
+
+            Anni = AnnoAtt - AnnoNascita;
+        }
     }
 }
