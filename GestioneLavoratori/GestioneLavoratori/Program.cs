6c98798510368a61b97079a19b55b7c4334f040f
PieroBagaggiolo <56152505+PieroBagaggiolo@users.noreply.github.com>
2019-10-08 10:37:35 +0200

    initialize input function

--- GestioneLavoratori/GestioneLavoratori/Program.cs
+++ GestioneLavoratori/GestioneLavoratori/Program.cs
@@ -10,7 +10,7 @@ namespace GestioneLavoratori
     {
         static void Main(string[] args)
         {
-            Autonomi a1 = new Autonomi("Paolo", "Verdi", 12, 1200, new DateTime(1960, 12, 1), new DateTime(1981, 11, 21))
+          /*  Autonomi a1 = new Autonomi("Paolo", "Verdi", 12, 1200, new DateTime(1960, 12, 1), new DateTime(1981, 11, 21))
             {
                 Genere = "M",
             };
@@ -27,7 +27,29 @@ namespace GestioneLavoratori
                 );
             Console.WriteLine("I dettagli del lavoratore d1 sono: " + 
                 System.Environment.NewLine + d1.GetDettaglioPersona()+ 
-                System.Environment.NewLine + d1.GetFinancial());
+                System.Environment.NewLine + d1.GetFinancial());*/
+
+            string scelta;
+            int quanto;
+            Console.WriteLine("Vuoi inserire lavoratori dipendenti (d) o lavoratori autonomi (a)?");
+            scelta = Console.ReadLine();
+            do
+            {
+                if (scelta == "d")
+                {
+
+                }
+                else if (scelta == "a")
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("L'opzione non è contemplata." + Environment.NewLine + "Inserisci (a) per lavoratori autonomi " +
+                        "oppure (d) per lavoratori dipendenti");
+                }
+            }
+            while (scelta != "d" || scelta != "a");
 
             Console.ReadLine();
         }
